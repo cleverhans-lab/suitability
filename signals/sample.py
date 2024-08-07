59abdadd44ba721b6345fb3828a851ad204c300c
Angéline Pouget <angeline.pouget@pouget.ch>
2024-08-07 15:58:16 -0400

    adding initial sample-level signals

--- signals/sample.py
+++ signals/sample.py
@@ -18,6 +18,7 @@ class SampleSignal:
     def evaluate(self):
         raise NotImplementedError("Subclasses should implement this method")
     
+
 class ConfidenceSignal(SampleSignal):
     '''
     Signal that evaluates the confidence of a model on a sample level.
@@ -33,7 +34,12 @@ class ConfidenceSignal(SampleSignal):
                 confidences.append(F.softmax(outputs, dim=1).max(dim=1)[0].cpu().numpy())
         return np.concatenate(confidences)
     
+
 class DecisionBoundarySignal(SampleSignal):
+    '''
+    Signal that evaluates the distance of a sample to the decision boundary of a model.
+    Loosely based on https://github.com/cleverhans-lab/dataset-inference/blob/main/src/attacks.py.
+    '''
     def __init__(self, model, dataloader, device, max_steps, metric, perturbation):
         super().__init__(model, dataloader, device)
         self.max_steps = max_steps
@@ -42,5 +48,75 @@ class DecisionBoundarySignal(SampleSignal):
 
     def evaluate(self):
         assert self.metric in ["l1", "l2", "linf"], f"Metric {self.metric} not supported, pick one of ['l1', 'l2', 'linf']"
+
         self.model.eval()
+        max_mag_per_image = np.array([])
+
+        # Define noise
+        l1_noise = lambda X: torch.from_numpy(np.random.laplace(loc=0.0, scale=2*self.perturbation, size=X.shape)).float().to(self.device) 
+        l2_noise = lambda X: torch.normal(0, self.perturbation, size=X.shape).to(self.device)
+        linf_noise = lambda X: torch.empty_like(X).uniform_(-self.perturbation, self.perturbation).to(self.device)
+        noise = {"l1": l1_noise, "l2": l2_noise, "linf": linf_noise}
+        
+        with torch.no_grad():  # No need to calculate gradients for evaluation
+            for data in self.dataloader:
+                inputs, _ = data
+                inputs = inputs.to(self.device)
+                predictions = torch.argmax(self.model(inputs), dim=1)
+
+                max_step = torch.ones(inputs.size(0)).to(self.device)
+                remaining = torch.ones(inputs.size(0)).to(self.device).bool()
+                delta = noise[self.metric](inputs)
+                
+                for i in range(1, self.max_steps):
+                    preds = self.model(inputs+i*delta)
+                    remaining = remaining & (torch.max(preds.data, 1)[1] == predictions)
+                    max_step[remaining] = i*torch.ones_like(max_step[remaining])
+                    if remaining.sum() == 0:
+                        break
+                max_mag_per_image = np.append(max_mag_per_image, max_step.cpu().numpy())
+
+        return max_mag_per_image
+    
+
+class TrainingDynamicsSignal(SampleSignal):
+    '''
+    Signal that evaluates the training dynamics of a model on a sample level.
+    It does so by calculating the number of unique predictions made by the model across different checkpoints (training epochs).
+    '''
+    def __init__(self, model, dataloader, device, model_checkpoints):
+        super().__init__(model, dataloader, device)
+        self.model_checkpoints = model_checkpoints
+
+    def evaluate(self):
+        num_samples = len(self.dataloader.dataset)
+        num_models = len(self.model_checkpoints)
+
+        all_predictions = np.zeros((num_models, num_samples), dtype=int)
+        unique_predictions = np.zeros(num_samples, dtype=int)
+
+        # Load each model checkpoint and make predictions on the dataset
+        for i, checkpoint in enumerate(self.model_checkpoints):
+            model = self.model
+            model.load_state_dict(torch.load(checkpoint))
+            model.to(self.device)
+            model.eval()
+
+            sample_idx = 0 # Index of the current sample
+            with torch.no_grad():
+                for data in self.dataloader:
+                    inputs, _ = data
+                    inputs = inputs.to(self.device)
+                    outputs = model(inputs)
+                    predictions = torch.argmax(outputs, dim=1).cpu().numpy()
+
+                    all_predictions[i, sample_idx:sample_idx+len(predictions)] = predictions
+                    sample_idx += len(predictions)
+
+        # Calculate the number of unique predictions for each sample
+        for i in range(num_samples):
+            unique_predictions[i] = len(np.unique(all_predictions[:, i]))
+
+        return unique_predictions
+
         
