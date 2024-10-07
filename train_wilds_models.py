b964e107d4892bdd8ed2d5dea2007133d7788800
Angéline Pouget <angeline.pouget@pouget.ch>
2024-10-07 16:17:42 -0400

    wilds experiments

--- train_wilds_models.py
+++ train_wilds_models.py
@@ -2,7 +2,7 @@
 import time
 import os
 
-datasets = ["iwildcam", "fmow"]
+datasets = ["camelyon17"]
 
 if not os.path.isdir('./job_files'):
     os.mkdir('./job_files')
@@ -20,6 +20,6 @@ for data in datasets:
         fh.writelines("#SBATCH -w caballus\n")
         fh.writelines("#SBATCH --output=./out_err_slurm/%j.out\n")
         fh.writelines("#SBATCH --error=./out_err_slurm/%j.err\n")
-        fh.writelines(f"python wilds/examples/run_expt.py --dataset {data} --algorithm ERM --root_dir /mfsnic/u/apouget/data/ --log_dir /mfsnic/u/apouget/experiments/{data} --use_wandb --wandb_api_key_path /h/321/apouget/wandb_key.txt --wandb_kwargs project=WILDS name={data} \n")
+        fh.writelines(f"python wilds/examples/run_expt.py --dataset {data} --algorithm ERM --download --root_dir /mfsnic/u/apouget/data/ --log_dir /mfsnic/u/apouget/experiments/{data} --use_wandb --wandb_api_key_path /h/321/apouget/wandb_key.txt --wandb_kwargs project=WILDS name={data} \n")
     os.system("sbatch %s" %job_file)
     time.sleep(0.3)
